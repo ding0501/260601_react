2876d48555b1d9562c683ebd39677a6fd6650267
ding0501 <2309712485@qq.com>
2026-06-12 20:38:40 +0800

    fix: 修复混合内容问题

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -8,6 +8,25 @@ const Support = () => {
     "//152.136.182.210:12231/api/information/support",
   );
 
+  // 将外部 HTTP 图片链接替换为本地 HTTPS 链接
+  const replaceImageUrls = (html: string) => {
+    if (!html) return "";
+
+    // 替换 mac 目录下的图片
+    let fixedHtml = html.replace(
+      /http:\/\/152\.136\.182\.210:12231\/images\/mac\//g,
+      "/images/mac/",
+    );
+
+    // 如果还有其他目录的图片，继续添加替换规则
+    fixedHtml = fixedHtml.replace(
+      /http:\/\/152\.136\.182\.210:12231\/images\//g,
+      "/images/",
+    );
+
+    return fixedHtml;
+  };
+
   if (loading) {
     return <Skeleton />;
   }
@@ -19,11 +38,14 @@ const Support = () => {
       </div>
     );
   }
+
+  const fixedHtml = replaceImageUrls(data?.data || "");
+
   return (
     <div className="min-h-screen text-apple-text dark:text-apple-text-dark">
       <div
         className="p-4 text-xs text-gray-500"
-        dangerouslySetInnerHTML={{ __html: data?.data || "" }}
+        dangerouslySetInnerHTML={{ __html: fixedHtml }}
       ></div>
     </div>
   );
