12aa1e77b4f5bd68cfaca8ac8aec98a648d08d47
ding0501 <2309712485@qq.com>
2026-06-12 20:40:22 +0800

    fix: 修复混合内容问题

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -5,27 +5,8 @@ import useApiData from "@/hooks/useApiData";
 
 const Support = () => {
   const { data, loading, error } = useApiData<SupportData>(
-    "//152.136.182.210:12231/api/information/support",
+    "http://152.136.182.210:12231/api/information/support",
   );
-
-  // 将外部 HTTP 图片链接替换为本地 HTTPS 链接
-  const replaceImageUrls = (html: string) => {
-    if (!html) return "";
-
-    // 替换 mac 目录下的图片
-    let fixedHtml = html.replace(
-      /http:\/\/152\.136\.182\.210:12231\/images\/mac\//g,
-      "/images/mac/",
-    );
-
-    // 如果还有其他目录的图片，继续添加替换规则
-    fixedHtml = fixedHtml.replace(
-      /http:\/\/152\.136\.182\.210:12231\/images\//g,
-      "/images/",
-    );
-
-    return fixedHtml;
-  };
 
   if (loading) {
     return <Skeleton />;
@@ -38,14 +19,11 @@ const Support = () => {
       </div>
     );
   }
-
-  const fixedHtml = replaceImageUrls(data?.data || "");
-
   return (
     <div className="min-h-screen text-apple-text dark:text-apple-text-dark">
       <div
         className="p-4 text-xs text-gray-500"
-        dangerouslySetInnerHTML={{ __html: fixedHtml }}
+        dangerouslySetInnerHTML={{ __html: data?.data || "" }}
       ></div>
     </div>
   );
